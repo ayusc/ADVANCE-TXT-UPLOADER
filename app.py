00192faa2f7c3f6b5b4746e5167c0ba6cf4aa8db
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-05-08 11:26:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+import threading
+import time
+import requests
+import os 
+
+SITE_URL = os.getenv("SITE_URL")
+
+if SITE_URL is None:
+    raise ValueError("SITE_URL environment variable is not set.")
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Tushar'
+def root():
+    return jsonify(status="Running")
 
+@app.route('/health')
+def health():
+    return jsonify(status="Healthy")
+
+def ping_self():
+    while True:
+        try:
+            res = requests.get(f"https://{SITE_URL}/health")
+            if res.status_code != 200:
+                print(f"Self ping failed with status code {res.status_code}")
+            # else:
+            #     print("Self ping succeeded")
+        except Exception as e:
+            print(f"Error in self-ping: {e}")
+        time.sleep(60)
 
 if __name__ == "__main__":
-    app.run()
+    threading.Thread(target=ping_self, daemon=True).start()
+    app.run(host='0.0.0.0', port=8000)
