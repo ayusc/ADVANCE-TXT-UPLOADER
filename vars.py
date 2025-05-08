126b185783729180b53c91229f26c8dca5874b06
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-05-08 11:47:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 API_ID = int(environ.get("API_ID", "3395155")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "995f4d71be988af9186c8455bda5c5e3"") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "7859533098:AAFboSxgj2K5kC_barBTlMK4cwpGc7EF_hM") #Replace with your bot token
-SITE_URL = environ.get("SITE_URL", "{{KOYEB_PUBLIC_URL}}")
+SITE_URL = environ.get("SITE_URL", "{{KOYEB_PUBLIC_DOMAIN}}")
